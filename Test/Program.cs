ccf9e3b3300c8eadbfa1360baadce30a45868062
srv.deepak <srv.deepak@DEV-JUMP-02>
2025-09-01 11:46:37 +0400

    Add project files.

---
+++ Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;  // <-- Must be at the top
+
+namespace HelloWorldApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Jenkins CI/CD!");
+        }
+    }
+}
